9cba03d0f9da4b2d005b36552d9fc2b001958e09
s14k51 <s14k51@gmail.com>
2020-03-23 15:54:11 -0500

    feat: add configurable configs

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const variableDeclarations = require("./rules/variable-declarations");
-const cjsImports = require('./rules/cjs-imports')
-const cjsExports = require('./rules/cjs-exports')
-const functionDeclarations = require('./rules/function-declarations')
+const cjsImports = require('./rules/cjs-imports');
+const cjsExports = require('./rules/cjs-exports');
+const functionDeclarations = require('./rules/function-declarations');
+
+const rules = [
+  ...cjsImports,
+  ...cjsExports,
+  ...variableDeclarations,
+  ...functionDeclarations
+];
+
+function configure(type) {
+  return {
+    env: {
+      es6: true,
+      node: true
+    },
+    rules: {
+      "padding-line-between-statements": [
+        type,
+        ...rules
+      ]
+    }
+  };
+}
 
 module.exports = {
-  env: {
-    es6: true,
-    node: true
-  },
-  rules: {
-    "padding-line-between-statements": [
-      "error",
-      ...cjsImports,
-      ...cjsExports,
-      ...variableDeclarations,
-      ...functionDeclarations
-    ]
+  configs: {
+    warn: configure("warn"),
+    error: configure("error")
   }
 };
